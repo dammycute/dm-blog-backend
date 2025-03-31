99ef14d5c4eb7bde7d6b15dd3df1d5f7d8079244
Olawoore Damilola James <58667901+dammycute@users.noreply.github.com>
2025-03-31 18:26:33 +0100

    Add: Implement the postgres db hosted on supabase

--- blog_be/settings.py
+++ blog_be/settings.py
@@ -145,12 +145,26 @@ WSGI_APPLICATION = "blog_be.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('dbname'),
+        'USER': os.getenv('user'),
+        'PASSWORD': os.getenv('password'),
+        'HOST': os.getenv('host'),  # Use the IP or domain if on a remote server
+        'PORT': 6543,  # Default PostgreSQL port
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
+
+# print (os.getenv('dbname'))
 
 
 # Password validation
